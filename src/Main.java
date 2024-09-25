95359a0f1e78d3a99dd2647db4c5eea81a0d8589
Sacha <sacha.okus@gmail.com>
2024-09-25 14:07:12 +0100

    snap class added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Snap snapGame = new Snap("Snap Game");
+        snapGame.shuffleDeck();
+        snapGame.playSnap();
     }
 }
--- src/Snap.java
+++ src/Snap.java
@@ -1,2 +1,31 @@
-public class Snap {
+import java.util.Scanner;
+
+public class Snap extends CardGame {
+
+    public Snap(String name) {
+        super(name);
+    }
+
+    public void playSnap() {
+        Scanner scanner = new Scanner(System.in);
+        Card previousCard = null;
+
+        while (deckOfCards.size() > 0) {
+            System.out.println("Press Enter to deal a card...");
+            scanner.nextLine();
+            Card currentCard = dealCard();
+            System.out.println("Card dealt: " + currentCard);
+
+            if (previousCard != null && currentCard.getSymbol().equals(previousCard.getSymbol())) {
+                System.out.println("Snap! You win!");
+                break;
+            }
+            previousCard = currentCard;
+        }
+
+        if (deckOfCards.size() == 0) {
+            System.out.println("Game Over! No more cards in the deck.");
+        }
+        scanner.close();
+    }
 }
