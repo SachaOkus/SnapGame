4ef7abab25a7d49d5913b8d5277aa747f6513fea
Sacha <sacha.okus@gmail.com>
2024-09-30 09:23:11 +0100

    Updated Main class with new players

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        Player player1 = new Player("Alice");
+        Player player2 = new Player("Bob");
 
         Snap snapGame = new Snap("Snap Game");
         snapGame.shuffleDeck();
