291782ff63e89bc2a35452a0d6ca23dd5aee2a39
Sacha <sacha.okus@gmail.com>
2024-09-30 09:56:13 +0100

    The game now continues if the snap window isnt made, I also added Player 1 and 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Player player1 = new Player("Alice");
-        Player player2 = new Player("Bob");
+        Player player1 = new Player("Player1");
+        Player player2 = new Player("Player2");
 
-        Snap snapGame = new Snap("Snap Game");
+        Snap snapGame = new Snap("Snap Game", player1, player2);
         snapGame.shuffleDeck();
         snapGame.playSnap();
     }
--- src/Snap.java
+++ src/Snap.java
@@ -4,8 +4,15 @@ import java.util.TimerTask;
 
 public class Snap extends CardGame {
 
-    public Snap(String name) {
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+
+    public Snap(String name, Player player1, Player player2) {
         super(name);
+        this.player1 = player1;
+        this.player2 = player2;
+        this.currentPlayer = player1;  // Player 1 starts first
     }
 
     public void playSnap() {
@@ -13,28 +20,38 @@ public class Snap extends CardGame {
         Card previousCard = null;
 
         while (deckOfCards.size() > 0) {
-            System.out.println("Press Enter to deal a card...");
-            scanner.nextLine();
+            System.out.println(currentPlayer.getName() + "'s turn. Press Enter to deal a card...");
+            scanner.nextLine();  // Wait for the current player to press Enter to deal a card
+
             Card currentCard = dealCard();
             System.out.println("Card dealt: " + currentCard);
 
+            // Check if it's a snap opportunity (two consecutive cards with the same symbol)
             if (previousCard != null && currentCard.getSymbol().equals(previousCard.getSymbol())) {
-                System.out.println("Snap opportunity! Type 'snap' within 2 seconds to win.");
+                System.out.println("Snap opportunity for " + currentPlayer.getName() + "! Type 'snap' within 2 seconds to win or press Enter to continue.");
 
                 boolean snapResult = waitForSnap(scanner);
                 if (snapResult) {
-                    System.out.println("Snap! You win!");
+                    System.out.println("Snap! " + currentPlayer.getName() + " wins!");
+                    break;
                 } else {
-                    System.out.println("Too slow! You lose.");
+                    System.out.println("No snap! Continuing the game...");
                 }
-                break;
             }
+
+            // Set the previous card for the next iteration
             previousCard = currentCard;
+
+            // Alternate the player for the next turn
+            currentPlayer = (currentPlayer == player1) ? player2 : player1;
         }
 
+        // If no more cards are available in the deck
         if (deckOfCards.size() == 0) {
             System.out.println("Game Over! No more cards in the deck.");
         }
+
+        // Close the scanner to avoid resource leaks
         scanner.close();
     }
 
@@ -46,8 +63,9 @@ public class Snap extends CardGame {
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
+                // If no snap is entered within the time limit, notify the player
                 if (!snapSuccess[0]) {
-                    System.out.println("Time's up! You failed to type 'snap' in time.");
+                    System.out.println("Time's up! " + currentPlayer.getName() + " didn't type 'snap' in time.");
                 }
             }
         };
@@ -58,13 +76,14 @@ public class Snap extends CardGame {
         // Wait for the player's input
         String input = scanner.nextLine();
 
-        // If the player types "snap" before the timer finishes
+        // If the player types "snap" within 2 seconds
         if (input.equalsIgnoreCase("snap")) {
             snapSuccess[0] = true;
             timer.cancel();  // Cancel the timer since the player succeeded
             return true;
         }
 
+        // If the player presses Enter or doesn't type "snap", the game continues
         return false;
     }
 }
