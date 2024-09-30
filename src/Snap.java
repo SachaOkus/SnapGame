7b5e8e2f23ffc63e904cfd9f1417fa0ac61dc29a
Sacha <sacha.okus@gmail.com>
2024-09-30 09:28:07 +0100

    2 second snap window added

--- src/Snap.java
+++ src/Snap.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Snap extends CardGame {
 
@@ -17,7 +19,14 @@ public class Snap extends CardGame {
             System.out.println("Card dealt: " + currentCard);
 
             if (previousCard != null && currentCard.getSymbol().equals(previousCard.getSymbol())) {
-                System.out.println("Snap! You win!");
+                System.out.println("Snap opportunity! Type 'snap' within 2 seconds to win.");
+
+                boolean snapResult = waitForSnap(scanner);
+                if (snapResult) {
+                    System.out.println("Snap! You win!");
+                } else {
+                    System.out.println("Too slow! You lose.");
+                }
                 break;
             }
             previousCard = currentCard;
@@ -27,5 +36,35 @@ public class Snap extends CardGame {
             System.out.println("Game Over! No more cards in the deck.");
         }
         scanner.close();
+    }
+
+    private boolean waitForSnap(Scanner scanner) {
+        final boolean[] snapSuccess = {false};
+
+        // Create a new Timer that counts down from 2 seconds
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                if (!snapSuccess[0]) {
+                    System.out.println("Time's up! You failed to type 'snap' in time.");
+                }
+            }
+        };
+
+        // Schedule the task to run after 2 seconds (2000 milliseconds)
+        timer.schedule(task, 2000);
+
+        // Wait for the player's input
+        String input = scanner.nextLine();
+
+        // If the player types "snap" before the timer finishes
+        if (input.equalsIgnoreCase("snap")) {
+            snapSuccess[0] = true;
+            timer.cancel();  // Cancel the timer since the player succeeded
+            return true;
+        }
+
+        return false;
     }
 }
